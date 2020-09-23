7b325ffb8f1e0683aee17de3ffb186df7628bb83
Nico Watine <nico@studioncreations.com>
2020-09-23 11:59:50 -0500

    Commit

--- src/pages/index.php
+++ src/pages/index.php
@@ -1,10 +1,10 @@
 ---
 url: /
-title:
-description:
-og-title:
-og-description:
-og-image:
+title: Jardin de France | Houston's French Antiques and Plants for the Home &amp; Patio
+description: Bringing to Houston the beauty of original French Country antiques &amp; quaint garden design
+og-title: Jardin de France - featured in MILIEU-MAG.com
+og-description: We are featured in the Spring 2017 publication of MILIEU! The full article is available on our website.
+og-image: https://jardindefrancetx.com/img/social-media/milieu/og-image.png
 body-id: index
 body-class: index
 ---
