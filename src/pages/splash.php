a2ef5868edff6480831ae2dee4e12d8ca137702d
Nico Watine <nico@studioncreations.com>
2020-09-29 10:34:08 -0500

    Tagline markup change

--- src/pages/splash.php
+++ src/pages/splash.php
@@ -5,27 +5,10 @@ description: Bringing to Houston the beauty of original French Country antiques 
 og-title: Jardin de France - featured in MILIEU-MAG.com
 og-description: We are featured in the Spring 2017 publication of MILIEU! The full article is available on our website.
 og-image: https://jardindefrancetx.com/img/social-media/milieu/og-image.png
-body-id: index
-body-class: index
+body-id: splash
+body-class: splash
 ---
-<section class="splash bgvid-container" id="welcome">
-	<div class="prose white display-flex position-absolute">
-		<div class="row expanded text-center">
-			<div class="px-32 mb-32">
-				<h1 class="cursive">Jardin de France</h1>
-			</div>
-			<div class="columns">
-				<h2 class="h3"><strong>French Antiques and Plants for the Home&nbsp;&amp;&nbsp;Patio</strong></h2>
-				<h3 class="h4">Bringing to Houston the beauty of original French Country antiques &amp; quaint garden&nbsp;design</h3>
-				<a href="#gardens" class="button cursive h4" data-scroll>Come Visit Us!</a>
-			</div>
-		</div>
-	</div>
-
-	<div class="bgScrim">&nbsp;</div>
-
-	<video autoplay playsinline muted loop poster="/img/index/splash/poster.jpg" id="backgroundVideo">
-		<source src="/img/index/splash/splash.webm" type="video/webm">
-		<source src="/img/index/splash/splash.mp4" type="video/mp4">
-	</video>
+<section class="hero-splash" id="welcome">
+	<h2 class="h2-tagline">French Antiques and Plants for the Home &amp; Patio</h2>
+	<h1 class="h1-headline">Jardin de France <span class="slogan">l’esprit français</span></h1>
 </section>
