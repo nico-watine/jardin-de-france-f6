23b54171ddca766e9c7d5c118a74068fdc0b4fc9
Nico Watine <nico@studioncreations.com>
2020-09-29 09:03:24 -0500

    New dev page for splash

---
+++ src/pages/splash.php
@@ -0,0 +1,50 @@
+---
+url: /
+title: Jardin de France | Houston's French Antiques and Plants for the Home &amp; Patio
+description: Bringing to Houston the beauty of original French Country antiques &amp; quaint garden design
+og-title: Jardin de France - featured in MILIEU-MAG.com
+og-description: We are featured in the Spring 2017 publication of MILIEU! The full article is available on our website.
+og-image: https://jardindefrancetx.com/img/social-media/milieu/og-image.png
+body-id: index
+body-class: index
+---
+<header>
+	<div class="row collapse">
+		<div class="logo small-3 columns">
+			<a href="#welcome" data-scroll><img src="/img/assets/logo/logo.png" class="pot" alt="Jardin de France Logo Icon"></a>
+			<a href="#welcome" data-scroll><h1 class="pt-8 pl-16 cursive">JdF</h1></a>
+		</div>
+		<nav class="nav-collapse">
+			<ul>
+				<li class="menu-item active" id="first"><a href="#welcome" data-scroll>Welcome</a></li>
+				<li class="menu-item"><a href="#gardens" data-scroll>The Gardens</a></li>
+				<li class="menu-item"><a href="#conservatory" data-scroll>The Conservatory</a></li>
+				<li class="menu-item"><a href="#interior-gallery" data-scroll>The Interior Gallery</a></li>
+				<li class="menu-item"><a href="#about" data-scroll>Our Story</a></li>
+				<li class="menu-item"><a href="#location" data-scroll>Location</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
+
+<section class="splash bgvid-container" id="welcome">
+	<div class="prose white display-flex position-absolute">
+		<div class="row expanded text-center">
+			<div class="px-32 mb-32">
+				<h1 class="cursive">Jardin de France</h1>
+			</div>
+			<div class="columns">
+				<h2 class="h3"><strong>French Antiques and Plants for the Home&nbsp;&amp;&nbsp;Patio</strong></h2>
+				<h3 class="h4">Bringing to Houston the beauty of original French Country antiques &amp; quaint garden&nbsp;design</h3>
+				<a href="#gardens" class="button cursive h4" data-scroll>Come Visit Us!</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="bgScrim">&nbsp;</div>
+
+	<video autoplay playsinline muted loop poster="/img/index/splash/poster.jpg" id="backgroundVideo">
+		<source src="/img/index/splash/splash.webm" type="video/webm">
+		<source src="/img/index/splash/splash.mp4" type="video/mp4">
+	</video>
+</section>
