7aaaaf9ebb0b402d16eaabe437b3a4b43900ea28
Nico Watine <nico@studioncreations.com>
2020-09-29 10:41:25 -0500

    Vertical spacing of text

--- src/pages/splash.php
+++ src/pages/splash.php
@@ -9,6 +9,6 @@ body-id: splash
 body-class: splash
 ---
 <section class="hero-splash" id="welcome">
-	<h2 class="h2-tagline">French Antiques and Plants for the Home &amp; Patio</h2>
-	<h1 class="h1-headline">Jardin de France <span class="slogan">l’esprit français</span></h1>
+	<h2 class="h2-tagline" style="padding-left: 130px;">French Antiques and Plants for the Home &amp; Patio</h2>
+	<h1 class="h1-headline" style="margin-top: -20px;">Jardin de France <span class="slogan" style="padding-left: 80px; margin-top: -40px;">l’esprit français</span></h1>
 </section>
