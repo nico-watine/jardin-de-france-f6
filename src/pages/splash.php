11f212db5fb8bd32cd44da93c85b2da677231d65
Nico Watine <nico@studioncreations.com>
2020-09-29 12:56:16 -0500

    Got it to bottom

--- src/pages/splash.php
+++ src/pages/splash.php
@@ -8,20 +8,20 @@ og-image: https://jardindefrancetx.com/img/social-media/milieu/og-image.png
 body-id: splash
 body-class: splash
 ---
-<section class="hero-splash" id="welcome">
-	<hgroup style="padding-left: 80px; padding-top: 130px;">
+<section class="hero-splash grid-x grid-padding-x" id="welcome">
+	<div style="padding-left: 80px; padding-top: 130px;">
 		<h2 class="h2-tagline" style="padding-left: 130px;">French Antiques and Plants for the Home &amp; Patio</h2>
 		<h1 class="h1-headline" style="margin-top: -20px;">Jardin de France <span class="slogan" style="padding-left: 80px; margin-top: -40px;">l’esprit français</span></h1>
-	</hgroup>
+	</div>
 
-	<header class="row expanded" data-sticky-container>
+	<div class="cell align-self-bottom" style="background: #fff;" data-sticky-container>
 		<div class="row">
-			<ul class="menu">
+			<ul class="menu align-right">
 				<li><a href="#welcome" data-scroll>Welcome</a></li>
 				<li><a href="#gardens" data-scroll>The Gardens</a></li>
 			</ul>
 		</div>
-	</header>
+	</div>
 </section>
 
 <section class="gardensSection" id="gardens" data-magellan-target="gardens" style="height: 2000px;">
