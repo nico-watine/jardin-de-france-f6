b954c3c754acbb38730c7a5e713e0c8fdbb05f4b
Nico Watine <nico@studioncreations.com>
2020-10-04 13:01:13 -0500

    Clean up demo markup

--- src/pages/index.php
+++ src/pages/index.php
@@ -36,34 +36,19 @@ body-class: index
 
 <div class="hide-for-large">
 
-<div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
-	<button class="menu-icon" type="button" data-toggle="example-menu"></button>
+<div class="title-bar text-right" data-responsive-toggle="example-menu" data-hide-for="medium">
 	<div class="title-bar-title">Menu</div>
+	<button class="menu-icon" type="button" data-toggle="example-menu"></button>
 </div>
 
-<div class="top-bar" id="example-menu">
-	<div class="top-bar-left">
-		<ul class="dropdown menu" data-dropdown-menu>
-			<li class="menu-text">Site Title</li>
-			<li>
-				<a href="#">One</a>
-				<ul class="menu vertical">
-					<li><a href="#">One</a></li>
-					<li><a href="#">Two</a></li>
-					<li><a href="#">Three</a></li>
-				</ul>
-			</li>
-			<li><a href="#">Two</a></li>
-			<li><a href="#">Three</a></li>
-		</ul>
-	</div>
-	<div class="top-bar-right">
-		<ul class="menu">
-			<li><input type="search" placeholder="Search"></li>
-			<li><button type="button" class="button">Search</button></li>
-		</ul>
-	</div>
+<div class="top-bar text-right" id="example-menu">
+	<ul class="menu vertical">
+		<li><a href="#0">One</a></li>
+		<li><a href="#0">Two</a></li>
+		<li><a href="#0">Three</a></li>
+	</ul>
 </div>
+
 
 <section class="splash bgvid-container" id="welcome">
 	<div class="prose white display-flex position-absolute">
