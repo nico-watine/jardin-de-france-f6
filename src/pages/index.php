46c0a0befdc788c4500170aa93691f3f8e293b8d
Nico Watine <nico@studioncreations.com>
2020-10-11 09:28:15 -0500

    Fix markup so deeplinking functions

--- src/pages/index.php
+++ src/pages/index.php
@@ -22,7 +22,7 @@ body-class: index
 					<a href="#welcome" data-magellan-target="welcome"><img src="/img/assets/logo/logo.png" class="pot" alt="Jardin de France Logo Icon"></a>
 					<a href="#welcome" data-magellan-target="welcome"><h1 class="pt-8 pl-16 cursive">JdF</h1></a>
 				</div>
-				<ul class="menu align-right" data-smooth-scroll data-animation-easing="swing" data-magellan data-deep-linking="true" data-offset="24">
+				<ul class="menu align-right" data-magellan data-deep-linking="true" data-offset="24">
 					<li><a href="#gardens">The Gardens</a></li>
 					<li><a href="#conservatory">The Conservatory</a></li>
 					<li><a href="#interior-gallery">The Interior Gallery</a></li>
@@ -38,8 +38,8 @@ body-class: index
 	<div class="position-fixed-top">
 		<div class="title-bar align-right" data-responsive-toggle="main-menu" data-hide-for="medium">
 			<div class="logo">
-				<a class="magellan-link" href="#splash" data-magellan-target="splash"><img src="/img/assets/logo/logo.png" class="pot" alt="Jardin de France Logo Icon"></a>
-				<a class="magellan-link" href="#splash" data-magellan-target="splash"><h1 class="pt-8 pl-16 cursive">JdF</h1></a>
+				<a class="magellan-link" href="#splash"><img src="/img/assets/logo/logo.png" class="pot" alt="Jardin de France Logo Icon"></a>
+				<a class="magellan-link" href="#splash"><h1 class="pt-8 pl-16 cursive">JdF</h1></a>
 			</div>
 			<div class="title-bar-title">Menu</div>
 			<button class="menu-icon" type="button" data-toggle="main-menu"></button>
@@ -48,11 +48,11 @@ body-class: index
 		<div class="top-bar align-right text-right" id="main-menu">
 			<div class="top-bar-left show-for-medium">
 				<div class="logo-wrap">
-					<a class="magellan-link" href="#splash" data-magellan-target="splash"><img src="/img/assets/logo/logo.png" class="pot" alt="Jardin de France Logo Icon"></a>
-					<a class="magellan-link" href="#splash" data-magellan-target="splash"><h1 class="cursive">JdF</h1></a>
+					<a class="magellan-link" href="#splash"><img src="/img/assets/logo/logo.png" class="pot" alt="Jardin de France Logo Icon"></a>
+					<a class="magellan-link" href="#splash"><h1 class="cursive">JdF</h1></a>
 				</div>
 			</div>
-			<ul class="menu vertical medium-horizontal" data-smooth-scroll data-animation-easing="swing" data-magellan data-deep-linking="true">
+			<ul class="menu vertical medium-horizontal" data-magellan data-deep-linking="true">
 				<li><a class="magellan-link" href="#gardens"><span>The </span>Gardens</a></li>
 				<li><a class="magellan-link" href="#conservatory"><span>The </span>Conservatory</a></li>
 				<li><a class="magellan-link" href="#interior-gallery"><span>The </span>Interior Gallery</a></li>
