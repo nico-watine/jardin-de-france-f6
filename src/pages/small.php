afb900d164f8d6275c6788b4c314d39d9dbe6dd1
Nico Watine <nico@studioncreations.com>
2021-03-02 16:42:28 -0600

    Progress on a temp page with temp scss

---
+++ src/pages/small.php
@@ -0,0 +1,68 @@
+---
+url: /
+title: Jardin de France | Houston's French Antiques and Plants for the Home & Patio
+description: Bringing to Houston the beauty of original French Country antiques & quaint garden design
+og-title: Jardin de France - featured in MILIEU-MAG.com
+og-description: We are featured in the Spring 2017 publication of MILIEU! The full article is available on our website.
+og-image: /img/social-media/milieu/og-image.png
+body-id: index
+body-class: index
+---
+<!-- Top Nav Bar -->
+<div class="hide-for-large" id="splash">
+	<div class="position-fixed-top">
+		<div class="title-bar align-right" data-responsive-toggle="main-menu" data-hide-for="medium">
+			<div class="logo">
+				<a class="magellan-link" href="#splash"><img src="/img/assets/logo/logo.png" class="pot" alt="Jardin de France Logo Icon"></a>
+				<a class="magellan-link" href="#splash"><h1 class="pt-8 pl-16 cursive">JdF</h1></a>
+			</div>
+			<div class="title-bar-title">Menu</div>
+			<button class="menu-icon" type="button" data-toggle="main-menu"></button>
+		</div>
+
+		<div class="top-bar align-right text-right" id="main-menu">
+			<div class="top-bar-left show-for-medium">
+				<div class="logo-wrap">
+					<a class="magellan-link" href="#splash"><img src="/img/assets/logo/logo.png" class="pot" alt="Jardin de France Logo Icon"></a>
+					<a class="magellan-link" href="#splash"><h1 class="cursive">JdF</h1></a>
+				</div>
+			</div>
+			<ul class="menu vertical medium-horizontal" data-magellan data-deep-linking="true">
+				<li><a class="magellan-link" href="#gardens"><span>The </span>Gardens</a></li>
+				<li><a class="magellan-link" href="#conservatory"><span>The </span>Conservatory</a></li>
+				<li><a class="magellan-link" href="#interior-gallery"><span>The </span>Interior Gallery</a></li>
+				<li><a class="magellan-link" href="#about">Our Story</a></li>
+				<li><a class="magellan-link" href="#location">Location</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<!-- <section class="splash bgvid-container" id="welcome">
+	<div class="prose white display-flex position-absolute">
+		<div class="row expanded text-center">
+			<div class="px-32">
+				<h1 class="cursive">Jardin de France</h1>
+			</div>
+			<div class="columns">
+				<h2 class="h3 ff-museo-500"><strong>l’esprit français</strong></h2>
+				<h3 class="h4">Curated for the home &amp; garden</h3>
+				<a href="#gardens" class="button cursive h4" data-scroll>Come Visit Us!</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="bgScrim">&nbsp;</div>
+
+	<video autoplay playsinline muted loop poster="/img/index/splash/poster.jpg" id="backgroundVideo">
+		<source src="/img/index/splash/splash.webm" type="video/webm">
+		<source src="/img/index/splash/splash.mp4" type="video/mp4">
+	</video>
+</section> -->
+
+<section class="small-splash grid-x align-middle grid-padding-x" style="min-height: 500px;">
+	<div class="cell text-center">
+		<h1 class="ff-parisienne text-white" style="text-shadow: 0 0 10px rgba(0,0,0,0.50); margin-bottom: 40px; font-size: 70px; line-height: 1;">Jardin de France <span class="display-block ff-museo-500" style="padding-left: 40px; font-size: 25px;">l’esprit français</span></h1>
+		<h2 class="ff-museo-500 text-white" style="text-shadow: 0 0 10px rgba(0,0,0,0.50); font-size: 18px;">Curated for the home &amp; garden</h2>
+	</div>
+</section>
