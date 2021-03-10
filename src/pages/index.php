8ba62dd61ab537c77c27fc28ae20ca41d2ed9a60
Nico Watine <nico@studioncreations.com>
2021-03-10 10:11:22 -0600

    Tweak og-meta

--- src/pages/index.php
+++ src/pages/index.php
@@ -3,7 +3,7 @@ url: /
 title: Jardin de France | l’Esprit Français for the Home & Garden
 description: Bringing to Houston the beauty of original French Country antiques, curated for the home and garden
 og-title: Jardin de France - featured in MILIEU-MAG.com
-og-description: We are featured in the Spring 2017 publication of MILIEU! The full article is available on our website.
+og-description: Come visit us in Round Top at Excess 2 and Bader Ranch!
 og-image: /img/social-media/milieu/og-image.png
 body-id: index
 body-class: index
