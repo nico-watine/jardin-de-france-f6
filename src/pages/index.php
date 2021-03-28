3f0fcb8ce168f17c42cee30e855b1d12fb4adbc9
Nico Watine <nico@studioncreations.com>
2021-03-28 10:07:39 -0500

    New google maps link to vendor site, not gps

--- src/pages/index.php
+++ src/pages/index.php
@@ -92,7 +92,7 @@ body-class: index
 				<div class="vendor-logos">
 					<div class="display-inline-block text-center logo-wrap" id="bader-ranch">
 						<!-- <a href="https://www.roundtopvendors.com" target="_blank"> --><img class="display-block bader-ranch-logo mb-16" src="/img/vendors/bader-ranch-logo.png" alt="Bader Ranch Logo" title="Bader Ranch Logo"><!-- </a> -->
-						<a href="https://goo.gl/maps/W97vj8vEEpqNhE5p7" class="large button ff-museo-500">Directions</a>
+						<a href="https://goo.gl/maps/GzRYnQRtXnGazxSY8" class="large button ff-museo-500">Directions</a>
 					</div>
 					<div class="display-inline-block text-center logo-wrap" id="excess-field">
 						<!-- <a href="https://excessfield.com" target="_blank"> --><img class="display-block excess-field-logo mb-16" src="/img/vendors/excess-field-logo.png" alt="Excess Field Logo" title="Excess Field Logo"><!-- </a> -->
