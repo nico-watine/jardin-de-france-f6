435cee512df5b05dc36abd5ade4387225a20365a
Nico Watine <nico@studioncreations.com>
2022-02-24 13:45:22 -0600

    Markup complete, prepare to merge w index

--- src/pages/location.php
+++ src/pages/location.php
@@ -11,13 +11,13 @@ body-class: index
 <!-- Location Section -->
 <section class="location-section" id="location" data-magellan-target="location">
 	<div class="row">
-		<div class="medium-11 columns medium-push-1">
-			<div class="prose-wrap float-center" style="max-width: 460px;">
+		<div class="large-11 columns large-push-1">
+			<div class="prose-wrap">
 				<div class="heading-wrap">
 					<h3 class="ff-museo-500">Important Update</h3>
-					<h2 style="line-height:  1;font-size: 60px;letter-spacing: -0.2px;text-align: center;" class="h1 ff-parisienne">Houston Location</h2>
+					<h2 class="h1 ff-parisienne">Houston Location</h2>
 				</div>
-				<div class="prose float-center" style="padding-left: 20px; padding-right: 20px;">
+				<div class="prose float-center">
 					<p>Local construction on Shepherd and Durham Dr has made our parking and access very limited. Because of this, our Houston location will be temporarily closing as of Saturday, February 26, until Fall 2022. We will be taking advantage of this time to remodel.</p>
 					<p>During our closure, private appointments will be available. Simply call ahead and we will schedule a day dedicated to your visit. Additionally, we can always be visited at our Round Top locations. For more information, we are only a call away. We greatly apologize for the inconvenience.</p>
 				</div>
