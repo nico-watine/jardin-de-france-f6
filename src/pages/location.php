13a72c9459e2fcc295789bfa0ab5de788602596b
Nico Watine <nico@studioncreations.com>
2022-02-24 13:12:56 -0600

    New partial to dev ssection

---
+++ src/pages/location.php
@@ -0,0 +1,10 @@
+---
+url: /
+title: Jardin de France | l’Esprit Français for the Home & Garden
+description: Bringing to Houston the beauty of original French Country antiques, curated for the home and garden
+og-title: Jardin de France - featured in MILIEU-MAG.com
+og-description: Come visit us in Round Top at Excess 2 and Bader Ranch!
+og-image: /img/social-media/milieu/og-image.png
+body-id: index
+body-class: index
+---
