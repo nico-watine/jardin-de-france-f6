0c84209b914be551c25327f772bfa5959c658cfc
Nico Watine <nico@studioncreations.com>
2022-12-14 10:02:00 -0600

    Move old construction location files to _ignores

--- src/pages/index.php
+++ src/pages/index.php
@@ -82,24 +82,6 @@ body-class: index
 </section>
 
 <!-- Location Section -->
-<!-- <section class="location-section" id="location" data-magellan-target="location">
-	<div class="row">
-		<div class="large-11 columns large-push-1">
-			<div class="prose-wrap">
-				<div class="heading-wrap">
-					<h3 class="ff-museo-500">Important Update</h3>
-					<h2 class="h1 ff-parisienne">Houston Location</h2>
-				</div>
-				<div class="prose float-center">
-					<p>Local construction on Shepherd and Durham Dr has made our parking and access very limited. Because of this, our Houston location will be temporarily closing as of Saturday, February 26, until Fall 2022. We will be taking advantage of this time to remodel.</p>
-					<p style="margin-bottom: 40px;">During our closure, private appointments will be available. Simply call ahead and we will schedule a day dedicated to your visit. Additionally, we can always be visited at our Round Top locations. For more information, we are only a call away. We greatly apologize for the inconvenience.</p>
-					<h3 class="ff-museo-500">832-651-3136 <br>
-					shop@jdftx.com</h3>
-				</div>
-			</div>
-		</div>
-	</div>
-</section> -->
 <section class="location-banner" id="location" data-magellan-target="location">
 	<div class="row">
 		<div class="medium-11 columns medium-push-1">
